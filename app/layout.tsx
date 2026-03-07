492e557daf1032b5aba8ae391e6469625e1bed8c
tigu77 <tigu7@naver.com>
2026-03-07 18:18:00 +0900

    네이버 서치어드바이저 인증코드 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,9 @@ export const metadata: Metadata = {
   },
   verification: {
     google: "sJctEFwOH0C-tk14N2XwoNkxFb99d_yG9Z32tLKFaYU",
+    other: {
+      "naver-site-verification": "48a255943fc3ff208b8e12c3feb80db997271acd",
+    },
   },
   robots: {
     index: true,
